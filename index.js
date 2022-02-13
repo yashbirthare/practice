feda371cc55836437817408cf48483f9e8413262
yashbirthare <95421356+yashbirthare@users.noreply.github.com>
2022-02-13 17:17:54 +0530

    done

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-var A = document.querySelector("#A")
-var B = document.querySelector("#B")
 var btn = document.querySelector("#btn")
-Button.addEventListener("click", eventhender)
 
-function eventhender() {
-    var input1 = A.value
-    var input2 = B.value
+button.addEventListener("Click", eventHeander)
 
-    console.log(input1 * input2)
-
+function eventHeander() {
+    console.log("clicked")
 }
