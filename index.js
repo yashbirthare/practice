4638461df323ba9b50d6b688af8d3e4e61b0d124
yashbirthare <95421356+yashbirthare@users.noreply.github.com>
2022-02-15 17:43:27 +0530

    done

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 var btn = document.querySelector("#btn");
-btn.addEventListener("click",clickEventHandler);
-function clickEventHandler () {
-    console.log("clicked!")
+var input1 = document.querySelector("#input1");
+var input2 = document.querySelector("#input2")
+var output= document.querySelector("#output");
+
+
+
+btn.addEventListener("click", clickEventHandler);
+function clickEventHandler(){
+
+    var ione = input1.value;
+    var itwo = input2.value;
+
+   output.innerText = ( ione * itwo) 
+   
 }
+console.log(one.value)
